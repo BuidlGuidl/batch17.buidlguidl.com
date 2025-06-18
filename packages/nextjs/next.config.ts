da1cd88d39eb536403c8febc4d7f386e5b9e1b12
Bobby Liu <bubby.liu0420@gmail.com>
2025-06-18 08:07:26 -0500

    Added Personal Page for Bobby Liu

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -14,6 +14,9 @@ const nextConfig: NextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
 
 const isIpfs = process.env.NEXT_PUBLIC_IPFS_BUILD === "true";
