862641af450375c263af14a04f44f474b8be80a8
Bobby Liu <bubby.liu0420@gmail.com>
2025-07-08 11:33:29 -0500

    Added Profile

--- packages/nextjs/components/MemberCard.tsx
+++ packages/nextjs/components/MemberCard.tsx
@@ -13,7 +13,7 @@ export interface Member {
 }
 
 export const MemberCard = ({ member }: { member: Member }) => (
-  <div className="bg-white dark:bg-gray-900 rounded-xl shadow-lg dark:shadow-indigo-900 p-5 sm:p-6 flex flex-col items-center text-left cursor-pointer transition h-full min-h-[400px]">
+  <div className="bg-white dark:bg-gray-900 rounded-xl shadow-lg dark:shadow-indigo-900 p-5 sm:p-6 flex flex-col items-center text-left transition h-full min-h-[400px]">
     <div className="w-20 h-20 sm:w-24 sm:h-24 rounded-full overflow-hidden border-4 border-indigo-400 dark:border-indigo-300 shadow-lg mb-4">
       <Image
         src={member.avatar}
@@ -23,15 +23,13 @@ export const MemberCard = ({ member }: { member: Member }) => (
         className="object-cover w-full h-full"
       />
     </div>
-    <Link
-      href={`/builders/${member.address}`}
-      className="font-bold text-lg sm:text-xl mb-2 text-gray-900 dark:text-white text-center hover:scale-[1.2]"
-    >
-      {member.name}
-    </Link>
+    <div className="font-bold text-lg sm:text-xl mb-2 text-gray-900 dark:text-white text-center">{member.name}</div>
     <Address address={member.address} />
     <div className="mb-4 text-sm text-gray-700 dark:text-gray-200 text-center min-h-[60px]">{member.description}</div>
     <div className="flex gap-3 sm:gap-4 mt-auto">
+      <Link href={`/builders/${member.address}`} className="hover:underline text-blue-500 dark:text-blue-300">
+        Profile
+      </Link>
       {member.github && (
         <a
           href={`https://github.com/${member.github}`}
